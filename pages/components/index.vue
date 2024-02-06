aa43e90ce7ee66e98a466dd1730e827b2cd58e07
Santo <santokhan1999@gmail.com>
2024-02-06 16:46:06 +0600

    Refactor

--- pages/components/index.vue
+++ pages/components/index.vue
@@ -7,23 +7,23 @@
                 <Button variant="white">START NOW</Button>
             </div>
             <div class="p-4 border rounded-lg flex flex-col gap-3 items-start bg-gray-100" title="Typography">
-                <Heading v-for="(item, index) in ['h6', 'h5', 'h4', 'h3', 'h2', 'h1']" :key="index" :variant="item">
+                <H v-for="(item, index) in ['h6', 'h5', 'h4', 'h3', 'h2', 'h1']" :key="index" :variant="item">
                     Typography
-                </Heading>
+                </H>
             </div>
             <div class="p-4 border rounded-lg flex flex-col gap-3 items-start bg-gray-100">
                 <div class="grid grid-cols-2 gap-4 items-start">
                     <div
                         class="col-span-2 p-6 rounded-lg bg-moo-gradient text-white flex items-center justify-between gap-4">
-                        <Heading variant="h3">JAMflow platform works with everything</Heading>
+                        <H variant="h3">JAMflow platform works with everything</H>
                         <Button variant="white">START NOW</Button>
                     </div>
                     <div class="space-y-6 p-6 rounded-lg bg-moo-gradient text-white">
-                        <Heading variant="h3">JAMflow platform works with everything</Heading>
+                        <H variant="h3">JAMflow platform works with everything</H>
                         <Button variant="white">START NOW</Button>
                     </div>
                     <div class="space-y-6 p-6 rounded-lg bg-moo-gradient text-white">
-                        <Heading variant="h3">See some references</Heading>
+                        <H variant="h3">See some references</H>
                         <Button variant="white">START NOW</Button>
                     </div>
                 </div>
@@ -32,7 +32,7 @@
                 <div class="w-full h-full grid place-items-center p-6 rounded-lg bg-moo-gradient text-white">
                     <div class="space-y-2 text-center">
                         <p>[ JAMflow LowCode/Framework ]</p>
-                        <Heading variant="h2">Build your website with our Framework</Heading>
+                        <H variant="h2">Build your website with our Framework</H>
                     </div>
                 </div>
             </div>
@@ -46,7 +46,7 @@
                                     d="M24 6V12M42 6V12M24 54V60M42 54V60M12 24H6M12 42H6M60 24H54M60 42H54M18 54H48C49.5913 54 51.1174 53.3679 52.2426 52.2426C53.3679 51.1174 54 49.5913 54 48V18C54 16.4087 53.3679 14.8826 52.2426 13.7574C51.1174 12.6321 49.5913 12 48 12H18C16.4087 12 14.8826 12.6321 13.7574 13.7574C12.6321 14.8826 12 16.4087 12 18V48C12 49.5913 12.6321 51.1174 13.7574 52.2426C14.8826 53.3679 16.4087 54 18 54ZM24 24H42V42H24V24Z"
                                     stroke="currentColor" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
                             </svg>
-                            <Heading variant="h3">Agency Lorem {{ item }}</Heading>
+                            <H variant="h3">Agency Lorem {{ item }}</H>
                             <p class="text-gray-300">
                                 Lorem ipsum dolor sit amet consectetur. Dignissim feugiat pellentesque vel dolor mauris
                                 mollis amet nunc. In porttitor pharetra molestie venenatis tortor vitae.
