6ff11159462a8a81e28117f512b15f5b498872ad
Santo <santokhan1999@gmail.com>
2024-02-08 12:41:16 +0600

    Update copyright

--- components/copyright.vue
+++ components/copyright.vue
@@ -1,7 +1,7 @@
 <template>
     <Container>
         <div class="flex flex-col gap-4 py-5 sm:flex-row sm:items-center sm:justify-between">
-            <NuxtLink to="/" class="text-3xl font-bold text-moo-bkg">Logo</NuxtLink>
+            <Logo />
             <span class="text-sm text-gray-500 sm:text-center text-moo-bkg">© 2023 <a class="hover:underline"
                     href="https://github.com/santokhan">Santo</a>. All
                 Rights Reserved.</span>
