ee6bff47984254c8fec33d421a6e8b5ca2cdf15b
Santo <santokhan1999@gmail.com>
2024-02-11 23:06:21 +0600

    Update footer

--- components/footer/footer.vue
+++ components/footer/footer.vue
@@ -1,17 +1,13 @@
 <template>
-    <footer class="text-moo-bkg">
-        <Container class="py-8 lg:py-14" v-if="footerNavs">
-            <div class="z-10 mx-auto flex max-w-screen-xl flex-wrap justify-between">
-                <div class="w-full py-2 sm:w-1/2 lg:w-2/12 lg:py-0" v-for="(nav, index) in footerNavs" :key="index">
-                    <FooterNavTitle>{{ nav.title }}</FooterNavTitle>
-                    <ul class="flex flex-col items-start">
-                        <FooterNavItem v-for="(link, index) in nav.links.slice(0, 3)" :key="index" :to="link.to">
-                            {{ link.name }}
-                        </FooterNavItem>
-                    </ul>
-                </div>
-            </div>
-        </Container>
+    <footer>
+        <UFooterColumns :links="footerNavs.map(nav => { nav.children = nav.children.slice(0, 3); return nav; })" :ui="{
+            wrapper: 'w-full flex flex-col lg:grid xl:grid-cols-1 gap-8 mx-auto max-w-screen-xl px-4 py-8 lg:py-14',
+            label: 'mb-2 font-bold uppercase text-moo-bkg text-base',
+            base: 'text-moo-bkg block py-1 font-medium transition-all ease-in text-base',
+            list: 'space-y-2 mt-4',
+            center: 'grid grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 grid-flow-row xl:col-span-1 gap-10',
+            left: 'mb-0'
+        }" />
         <Container>
             <div class="flex flex-col gap-4 py-5 sm:flex-row sm:items-center sm:justify-between">
                 <Logo />
--- components/footer/nav-title.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div class="mb-2 font-bold uppercase text-moo-bkg">
-        <slot></slot>
-    </div>
-</template>
