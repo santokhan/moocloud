e54a28f56dbadc8d418eafa75e8012b6081a584b
casualmatt <matteo@rezzin.ch>
2024-02-12 11:48:02 +0100

    fix: change img logo

--- components/logo.vue
+++ components/logo.vue
@@ -1,5 +1,5 @@
 <template>
     <NuxtLink to="/" class="hover:brightness-90">
-        <img src="/assets/images/logo/logo.svg" alt="logo" class="w-32" />
+        <img src="/images/Logo_Jamflow.png" alt="logo" class="w-32" />
     </NuxtLink>
 </template>
