1fdadb507fa16316429b6dcb91566faabcf88e26
Santo <santokhan1999@gmail.com>
2024-02-13 00:23:51 +0600

    update: pages

--- components/banner.vue
+++ components/banner.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="p-8 rounded-lg bg-moo-gradient text-white flex items-center justify-between gap-4">
+    <div class="p-8 rounded-lg bg-moo-gradient text-white flex flex-wrap items-center justify-between gap-4">
         <slot></slot>
     </div>
 </template>
--- pages/lowcode.vue
+++ pages/lowcode.vue
@@ -29,26 +29,31 @@
             </template>
             <template #features>
                 <FeatureGrid class="py-12">
-                    <FeatureCard v-for="(item, index) in features" :key="index">
-                        <IconFeature />
-                        <H variant="h4">{{ item.title }} {{ index }}</H>
-                        <Text>{{ item.description }}</Text>
-                    </FeatureCard>
+                    <ULandingCard v-for="n in 9" title="JAMflow 123" color="blue"
+                        description="Lorem ipsum dolor sit amet consectetur. Dignissim feugiat pellentesque vel dolor mauris mollis amet nunc. In porttitor pharetra molestie venenatis tortor vitae."
+                        icon="i-heroicons-swatch" :ui="{
+                            wrapper: 'relative group isolate rounded-xl background-gradient-moocloud before:hidden before:lg:block before:absolute ring-0 before:-inset-[2px] before:h-[calc(100%+4px)] before:w-[calc(100%+4px)] before:z-[-1] before:rounded-[13px] flex-1 flex flex-col shadow',
+                            base: 'w-full h-full rounded-lg bg-moo-dark-jam text-moo-bkg text-center flex flex-col items-center gap-2 border-0',
+                            background: 'bg-moo-dark-jam',
+                            container: '',
+                            to: 'hover:ring-2 hover:ring-red-500 dark:hover:ring-red-400 hover:bg-gray-100/50 dark:hover:bg-gray-800/50',
+                            icon: {
+                                wrapper: '',
+                                base: 'text-moo-bkg w-10 h-10'
+                            },
+                            title: 'text-moo-bkg',
+                            description: 'text-moo-bkg',
+                            ring: ''
+                        }" />
                 </FeatureGrid>
             </template>
         </Feature>
 
         <Container class="py-8 lg:py-14">
             <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
-                <Banner>
+                <Banner v-for="n in 2">
                     <H variant="h3" class="max-w-xl">
-                        A platform to build your network as agile as your business. See it in action
-                    </H>
-                    <Button variant="white" class="">taka a tour</Button>
-                </Banner>
-                <Banner>
-                    <H variant="h3" class="max-w-xl">
-                        A platform to build your network as agile as your business. See it in action
+                        A platform to build your network as agile
                     </H>
                     <Button variant="white" class="">taka a tour</Button>
                 </Banner>
