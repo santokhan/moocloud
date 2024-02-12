899a7218ba681ac18e12fc8780b7573b4395a53a
casualmatt <matteo@rezzin.ch>
2024-02-12 13:33:52 +0100

    Refactor services component to use dynamic classes

--- components/services.vue
+++ components/services.vue
@@ -1,8 +1,26 @@
 <template>
-    <Container class="py-8 lg:py-14 text-moo-bkg">
-        <div
-            class="grid grid-cols-1 lg:grid-cols-2 gap-y-12 border-4 lg:divide-x-4 divide-moo-blue-low border-moo-blue-low rounded-xl">
-            <slot></slot>
-        </div>
-    </Container>
+  <Container class="py-8 lg:py-14 text-moo-bkg">
+    <div
+      class="grid grid-cols-1 lg:grid-cols-2 gap-y-12  rounded-xl"
+      :class="{
+        'border-moo-blue-low border-4': border,
+        'divide-moo-blue-low lg:divide-x-4': divider,
+      }"
+    >
+      <slot></slot>
+    </div>
+  </Container>
 </template>
+
+<script setup lang="ts">
+const { divider, border } = defineProps({
+  divider: {
+    type: Boolean,
+    default: true,
+  },
+  border: {
+    type: Boolean,
+    default: true,
+  },
+});
+</script>
