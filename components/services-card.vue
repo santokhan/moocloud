8774fc65410cc8d45020bd5aa064b9ff1363ca62
Santo <santokhan1999@gmail.com>
2024-02-13 00:13:52 +0600

    update services

--- components/services-card.vue
+++ components/services-card.vue
@@ -1,7 +1,9 @@
 <template>
-    <div class="relative space-y-4 text-moo-bkg p-4 lg:p-10 h-full">
-        <slot name="image"></slot>
-        <div class="flex flex-col items-start gap-3 h-full">
+    <div class="relative space-y-4 text-moo-bkg h-full">
+        <div class="w-1/2">
+            <slot name="image"></slot>
+        </div>
+        <div class="flex flex-col items-start gap-2 h-full">
             <slot name="details"></slot>
         </div>
     </div>
--- components/services.vue
+++ components/services.vue
@@ -1,12 +1,9 @@
 <template>
   <Container class="py-8 lg:py-14 text-moo-bkg">
-    <div
-      class="grid grid-cols-1 lg:grid-cols-2 gap-y-12  rounded-xl"
-      :class="{
-        'border-moo-blue-low border-4': border,
-        'divide-moo-blue-low lg:divide-x-4': divider,
-      }"
-    >
+    <div class="grid grid-cols-1 lg:grid-cols-2 gap-12  rounded-xl" :class="{
+      'border-moo-blue-low border-4': border,
+      'divide-moo-blue-low lg:divide-x-4': divider,
+    }">
       <slot></slot>
     </div>
   </Container>
