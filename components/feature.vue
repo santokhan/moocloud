d36e72b9b8d3983b4a72245a5816ecb5a52fd706
Santo <santokhan1999@gmail.com>
2024-02-13 00:13:34 +0600

    update: feature spacing

--- components/feature.vue
+++ components/feature.vue
@@ -1,6 +1,6 @@
 <template>
     <Container class="py-8 lg:py-14">
-        <div class="space-y-2 text-moo-bkg">
+        <div class="space-y-12 text-moo-bkg">
             <div class="space-y-2 max-w-2xl">
                 <slot name="headings"></slot>
             </div>
